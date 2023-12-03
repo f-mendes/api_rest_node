32b9330b8a46fc8eb76e8c5ec87491e7f8a0dc40
Felipe <fsoares@autodoc.com.br>
2023-12-03 20:46:44 -0300

    feat: creating plugins for routes

---
+++ src/routes/transactions.ts
@@ -0,0 +1,26 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import knex from 'knex'
+import { randomUUID } from 'crypto'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
+  })
+}
